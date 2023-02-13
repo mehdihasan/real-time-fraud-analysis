cc4f013d3e4c240c10cba35c22ac3155aedfd268
mehdihasan <email2mehdihasan@gmail.com>
2023-02-13 15:43:36 +0100

    Update comments for ksql

--- apps/ksqldb.sql
+++ apps/ksqldb.sql
@@ -8,7 +8,11 @@ CREATE OR REPLACE STREAM fd_cust_raw_stream (
     ) WITH (KAFKA_TOPIC = 'v0.public.customers',
           VALUE_FORMAT = 'JSON');
 
+-- Use the following statement to query fd_cust_raw_stream stream to ensure it's being populated correctly.
+-- SELECT * FROM fd_cust_raw_stream EMIT CHANGES;
 
+
+-- Create fd_customers table based on the fd_cust_raw_stream stream.
 CREATE OR REPLACE TABLE fd_customers WITH (FORMAT='JSON') AS
     SELECT after->id                            AS customer_id,
            LATEST_BY_OFFSET(after->first_name)  AS first_name,
@@ -19,7 +23,11 @@ CREATE OR REPLACE TABLE fd_customers WITH (FORMAT='JSON') AS
     FROM    fd_cust_raw_stream
     GROUP BY after->id;
 
--- create transactions stream
+-- Use the following statement to query fd_customers table to ensure it's being populated correctly.
+-- SELECT * FROM fd_customers;
+
+
+-- create transactions stream from rabbitmq_transactions_v0 topic
 CREATE OR REPLACE STREAM fd_transactions(
 	userid INTEGER,
   	transaction_timestamp VARCHAR,
@@ -29,6 +37,10 @@ CREATE OR REPLACE STREAM fd_transactions(
   	credit_card_number VARCHAR
 	)
 WITH(KAFKA_TOPIC='rabbitmq_transactions_v0', KEY_FORMAT='JSON', VALUE_FORMAT='JSON', timestamp ='transaction_timestamp', timestamp_format = 'yyyy-MM-dd HH:mm:ss');
+
+-- Use the following statement to query fd_transactions stream to ensure it's being populated correctly.
+-- SELECT * FROM fd_transactions EMIT CHANGES;
+
 
 -- join transactions stream with customer table
 CREATE OR REPLACE STREAM fd_transactions_enriched WITH (KAFKA_TOPIC = 'transactions_enriched') AS
@@ -44,7 +56,13 @@ CREATE OR REPLACE STREAM fd_transactions_enriched WITH (KAFKA_TOPIC = 'transacti
   INNER JOIN fd_customers C
   ON T.USERID = C.CUSTOMER_ID;
 
--- trigger fraud alert when dollar amount exceeds known avg spend
+-- Use the following statement to query fd_transactions_enriched stream to ensure it's being populated correctly.
+-- SELECT * FROM fd_transactions_enriched EMIT CHANGES;
+
+
+-- Aggregate the stream of transactions for each account ID using a two-hour tumbling window,
+-- and filter for accounts in which the total spend in a two-hour period is greater than the customer’s average.
+-- Triggers fraud alert when dollar amount exceeds known avg spend
 CREATE OR REPLACE TABLE fd_possible_stolen_card WITH (KAFKA_TOPIC = 'FD_possible_stolen_card', KEY_FORMAT = 'JSON', VALUE_FORMAT='JSON') AS
   SELECT
     TIMESTAMPTOSTRING(WINDOWSTART, 'yyyy-MM-dd HH:mm:ss') AS WINDOW_START,
@@ -59,3 +77,6 @@ CREATE OR REPLACE TABLE fd_possible_stolen_card WITH (KAFKA_TOPIC = 'FD_possible
   WINDOW TUMBLING (SIZE 2 HOURS)
   GROUP BY T.USERID, T.CREDIT_CARD_NUMBER, T.FULL_NAME, T.EMAIL, T.TRANSACTION_TIMESTAMP
   HAVING SUM(T.AMOUNT) > MAX(T.AVG_CREDIT_SPEND);
+
+-- Use the following statement to query fd_possible_stolen_card table to ensure it's being populated correctly.
+-- SELECT * FROM fd_possible_stolen_card;
